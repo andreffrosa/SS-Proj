4b2d2fbcea7cd7e037c6bf583a712ffbdb3315ad
j.geraldo <j.geraldo@FCT.UNL.PT>
2018-11-17 21:08:54 +0000

    Create new interface component DoubleInputBox. Add interface support to the following functions: - Negative - RedChannel - BrightContrast - Translation - Rotation - Scale - Scale_point_xy - Rotation_Bilinear - Scale_Bilinear - Scale_point_xy_Bilinear

---
+++ SS_OpenCV/DoubleInputBox.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SS_OpenCV
+{
+    partial class DoubleInputBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ValueTextBox1 = new System.Windows.Forms.TextBox();
+            this.ValueTextBox2 = new System.Windows.Forms.TextBox();
+            this.ValueLabel1 = new System.Windows.Forms.Label();
+            this.ValueLabel2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ValueTextBox1
+            // 
+            this.ValueTextBox1.Location = new System.Drawing.Point(99, 43);
+            this.ValueTextBox1.Name = "ValueTextBox1";
+            this.ValueTextBox1.Size = new System.Drawing.Size(136, 22);
+            this.ValueTextBox1.TabIndex = 0;
+            // 
+            // ValueTextBox2
+            // 
+            this.ValueTextBox2.Location = new System.Drawing.Point(99, 81);
+            this.ValueTextBox2.Name = "ValueTextBox2";
+            this.ValueTextBox2.Size = new System.Drawing.Size(136, 22);
+            this.ValueTextBox2.TabIndex = 1;
+            // 
+            // ValueLabel1
+            // 
+            this.ValueLabel1.AutoSize = true;
+            this.ValueLabel1.Location = new System.Drawing.Point(10, 46);
+            this.ValueLabel1.Name = "ValueLabel1";
+            this.ValueLabel1.Size = new System.Drawing.Size(52, 17);
+            this.ValueLabel1.TabIndex = 2;
+            this.ValueLabel1.Text = "Value1";
+            // 
+            // ValueLabel2
+            // 
+            this.ValueLabel2.AutoSize = true;
+            this.ValueLabel2.Location = new System.Drawing.Point(10, 84);
+            this.ValueLabel2.Name = "ValueLabel2";
+            this.ValueLabel2.Size = new System.Drawing.Size(52, 17);
+            this.ValueLabel2.TabIndex = 3;
+            this.ValueLabel2.Text = "Value2";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(99, 125);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // DoubleInputBox
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 166);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ValueLabel2);
+            this.Controls.Add(this.ValueLabel1);
+            this.Controls.Add(this.ValueTextBox2);
+            this.Controls.Add(this.ValueTextBox1);
+            this.Name = "DoubleInputBox";
+            this.Text = "Double Input Box";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox ValueTextBox1;
+        public System.Windows.Forms.TextBox ValueTextBox2;
+        public System.Windows.Forms.Label ValueLabel1;
+        public System.Windows.Forms.Label ValueLabel2;
+        private System.Windows.Forms.Button button1;
+    }
+}
--- SS_OpenCV/MainForm.cs
+++ SS_OpenCV/MainForm.cs
@@ -11,10 +11,30 @@ namespace SS_OpenCV
         Image<Bgr, Byte> imgUndo = null; // undo backup image - UNDO
         string title_bak = "";
 
+        // Global Mouse variables
+        int mouseX, mouseY;
+        bool mouseFlag = false;
+
         public MainForm()
         {
             InitializeComponent();
             title_bak = Text;
+        }
+
+        /// <summary>
+        /// Specify program behaviour in case of an image mouse click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ImageViewer_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (mouseFlag)
+            {
+                mouseX = e.X;
+                mouseY = e.Y;
+
+                mouseFlag = false;
+            }
         }
 
         /// <summary>
@@ -177,9 +197,292 @@ namespace SS_OpenCV
 
             Cursor = Cursors.Default; // normal cursor 
         }
+
+        /// <summary>
+        /// Call image convertion to red channel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void redChannelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            ImageClass.RedChannel(img);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
+
+        /// <summary>
+        /// Change image according to the specified contrast and brightness
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void brightContrastToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+            
+            DoubleInputBox form = new DoubleInputBox("Brightness & Contrast", "Brightness:", "Contrast:");
+            form.ValueTextBox1.Text = "int from -255 to 255";
+            form.ValueTextBox2.Text = "float from 0 to 3";
+            form.ShowDialog();
+
+            bool numericBrightness = int.TryParse(form.ValueTextBox1.Text, out int brightness);
+            // Have to specify any number format and invariant culture to accept numbers separated by dots or commas
+            bool numericContrast = float.TryParse(form.ValueTextBox2.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out float contrast);
+
+            // Brightness and Contrast input values are restricted accordingly: -255 <= brightness <= 255 ; 0 <= contrast <= 3
+            if (!(numericBrightness && numericContrast && brightness >= -255 && brightness <= 255 && contrast >= 0 && contrast <= 3))
+                return;
+
+            ImageClass.BrightContrast(img, brightness , contrast);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
+
+        /// <summary>
+        /// Move image according to the specified x and y values
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void translationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            DoubleInputBox form = new DoubleInputBox("Translation", "X:", "Y:");
+            form.ValueTextBox1.Text = "pos/neg int";
+            form.ValueTextBox2.Text = "pos/neg int";
+            form.ShowDialog();
+            bool numericX = int.TryParse(form.ValueTextBox1.Text, out int x);
+            bool numericY = int.TryParse(form.ValueTextBox2.Text, out int y);
+
+            if (!(numericX && numericY))
+                return;
+
+            ImageClass.Translation(img, img.Copy(), x, y);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
+
+        /// <summary>
+        /// Rotate image according to the specified angle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void simpleRotationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            InputBox form = new InputBox("Angle (Deg)");
+            form.ValueTextBox.Text = "pos/neg float";
+            form.ShowDialog();
+            // Have to specify any number format and invariant culture to accept numbers separated by dots or commas
+            bool numericAngle = float.TryParse(form.ValueTextBox.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out float angle);
+
+            if (!numericAngle)
+                return;
+
+            angle = (float) ((Math.PI / 180.0) * angle);
+
+            ImageClass.Rotation(img, img.Copy(), angle);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
+
+        /// <summary>
+        /// Rotate image according to the specified angle, applying bilinear interpolation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bilinearRotationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            InputBox form = new InputBox("Angle (Deg)");
+            form.ValueTextBox.Text = "pos/neg float";
+            form.ShowDialog();
+            // Have to specify any number format and invariant culture to accept numbers separated by dots or commas
+            bool numericAngle = float.TryParse(form.ValueTextBox.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out float angle);
+
+            if (!numericAngle)
+                return;
+
+            angle = (float)((Math.PI / 180.0) * angle);
+
+            ImageClass.Rotation_Bilinear(img, img.Copy(), angle);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
+
+        /// <summary>
+        /// Resize image according to the specified scale factor
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void simpleScaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            InputBox form = new InputBox("Scale Factor");
+            form.ValueTextBox.Text = "positive float";
+            form.ShowDialog();
+            // Have to specify any number format and invariant culture to accept numbers separated by dots or commas
+            bool numericScaleFactor = float.TryParse(form.ValueTextBox.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out float scaleFactor);
+
+            if (!numericScaleFactor)
+                return;
+
+            ImageClass.Scale(img, img.Copy(), scaleFactor);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
+
+        /// <summary>
+        /// Resize image according to the specified scale factor, applying bilinear interpolation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bilinearScaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            InputBox form = new InputBox("Scale Factor");
+            form.ValueTextBox.Text = "positive float";
+            form.ShowDialog();
+            // Have to specify any number format and invariant culture to accept numbers separated by dots or commas
+            bool numericScaleFactor = float.TryParse(form.ValueTextBox.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out float scaleFactor);
+
+            if (!numericScaleFactor)
+                return;
+
+            ImageClass.Scale_Bilinear(img, img.Copy(), scaleFactor);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
+
+        /// <summary>
+        /// Resize image according to the specified scale factor and point
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void simpleScalePointToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            InputBox form = new InputBox("Scale Factor");
+            form.ValueTextBox.Text = "positive float";
+            form.ShowDialog();
+            // Have to specify any number format and invariant culture to accept numbers separated by dots or commas
+            bool numericScaleFactor = float.TryParse(form.ValueTextBox.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out float scaleFactor);
+
+            if (!numericScaleFactor)
+                return;
+
+            mouseFlag = true;
+            while (mouseFlag)
+                Application.DoEvents();
+
+            ImageClass.Scale_point_xy(img, img.Copy(), scaleFactor, mouseX, mouseY);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
+
+        /// <summary>
+        /// Resize image according to the specified scale factor and point, applying bilinear interpolation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bilinearScalePointToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            InputBox form = new InputBox("Scale Factor");
+            form.ValueTextBox.Text = "positive float";
+            form.ShowDialog();
+            // Have to specify any number format and invariant culture to accept numbers separated by dots or commas
+            bool numericScaleFactor = float.TryParse(form.ValueTextBox.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out float scaleFactor);
+
+            if (!numericScaleFactor)
+                return;
+
+            mouseFlag = true;
+            while (mouseFlag)
+                Application.DoEvents();
+
+            ImageClass.Scale_point_xy_Bilinear(img, img.Copy(), scaleFactor, mouseX, mouseY);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
     }
-
-
-
-
 }
