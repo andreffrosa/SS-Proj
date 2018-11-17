20343aba3dd787884720d3faed02dc9333089f84
David Carpinteiro <d.carpinteiro@campus.fct.unl.pt>
2018-11-17 21:14:38 +0000

    Improved piece corner discovery
    Update Puzzle Class

--- SS_OpenCV/Puzzle.cs
+++ SS_OpenCV/Puzzle.cs
@@ -7,7 +7,26 @@ namespace SS_OpenCV
 {
     internal class Puzzle
     {
-        public static uint[,] getLabels(Image<Bgr, byte> img)
+        const int MaxNumberPieces = 10;
+        const double RadToDegreeFactor = 180.0 / Math.PI;
+        private Image<Bgr, byte> img;
+        List<Image<Bgr, byte>> images_pieces;
+        uint[,] labels;
+        uint[] labels_index;
+        int curr_label_index;
+
+        public Puzzle(Image<Bgr, byte> img)
+        {
+            this.img = img;
+            labels = new uint[img.Width, img.Height];
+            labels_index = new uint[MaxNumberPieces];
+            curr_label_index = 0;
+            images_pieces = new List<Image<Bgr, byte>>();
+
+            this.getLabels();
+        }
+
+        private uint[,] getLabels()
         {
             unsafe
             {
@@ -21,7 +40,7 @@ namespace SS_OpenCV
                 int padding = m.widthStep - m.nChannels * m.width;
                 int step = m.widthStep;
 
-                uint[,] labels = new uint[img.Width, img.Height];
+
                 bool changed = true;
                 uint curr_label = 1;
 
@@ -45,7 +64,7 @@ namespace SS_OpenCV
                     dataPtrOriginal += padding;
                 }
 
-                // Cicle
+                // Cycle
                 while (changed)
                 {
                     changed = false;
@@ -153,170 +172,51 @@ namespace SS_OpenCV
                         dataPtrOriginal -= nChan * 2 + padding;
                     }
                 }
-
-                /*
-                dataPtrOriginal = originalPtr;
-                List<uint> pieces = new List<uint>();
-                for (int y = 0; y < height; y++)
-                {
-                    for (int x = 0; x < width; x++)
-                    {
-                        if (labels[x, y] != UInt32.MaxValue)
-                        {
-                            dataPtrOriginal[0] = (byte) (labels[x, y]*10 % 256);
-                            dataPtrOriginal[1] = 0;
-                            dataPtrOriginal[2] = (byte)(labels[x, y] * 10 % 256);
-                            if (!pieces.Contains(labels[x, y]))
-                            {
-                                pieces.Add(labels[x, y]);
-                               // Console.WriteLine(labels[x, y]);
- 
-                            }
-                        }
-                        dataPtrOriginal += nChan;
-                    }
-                    //at the end of the line advance the pointer by the aligment bytes (padding)
-                    dataPtrOriginal += padding;
-                }
-                Console.WriteLine(pieces.Count);
-                */
-
                 return labels;
             }
 
         }
 
-        public static void getPiecesPosition(Image<Bgr, byte> originalImage, uint[,] labels, out List<int[]> Pieces_positions, out List<int> Pieces_angle, out List<Image<Bgr, byte>> images_pieces)
+        private int getLabelIndex(uint label)
         {
-            int width = labels.GetLength(0);
-            int height = labels.GetLength(1);
-
-            Pieces_positions = new List<int[]>();
-            Pieces_angle = new List<int>();
-            List<double> angles_rads = new List<double>();
-            int[] piece_vector;
-            images_pieces = new List<Image<Bgr, byte>>();
-
-
-            // var map = new Dictionary<uint, int[]>(10);
-            uint[] values = new uint[10];
-            int curr_pos = 0;
-
-            int[] x_top_left = new int[10];
-            int[] y_top_left = new int[10];
-            int[] helper_point_x = new int[10];
-            int[] helper_point_y = new int[10];
-
-            for (int i = 0; i < 10; i++)
+            int index = Array.IndexOf(this.labels_index, label);
+            if (index == -1)
             {
-                x_top_left[i] = Int32.MaxValue;
-                y_top_left[i] = Int32.MaxValue;
-                helper_point_x[i] = -1;
-                helper_point_y[i] = -1;
+                labels_index[this.curr_label_index] = label;
+                index = this.curr_label_index++;
             }
-
-            int[] x_bottom_right = new int[10];
-            int[] y_bottom_right = new int[10];
-
-
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    if (labels[x, y] != UInt32.MaxValue)
-                    {
-                        int index = Array.IndexOf(values, labels[x, y]);
-                        if (index == -1)
-                        {
-                            values[curr_pos] = labels[x, y];
-                            index = curr_pos++;
-                        }
-                        if (helper_point_x[index] == -1)
-                        {
-                            helper_point_x[index] = x;
-                            helper_point_y[index] = y;
-                        }
-
-                        if (x_top_left[index] > x)
-                        {
-                            x_top_left[index] = x;
-                            y_top_left[index] = y;
-                        }
-                        if (x_bottom_right[index] <= x)
-                        {
-                            x_bottom_right[index] = x;
-                            y_bottom_right[index] = y;
-                        }
-                    }
-                }
-            }
-
-            for (int i = 0; i < curr_pos; i++)
-            {
-                piece_vector = new int[4];
-                piece_vector[0] = x_top_left[i];   // x- Top-Left 
-                piece_vector[1] = y_top_left[i];  // y- Top-Left
-                piece_vector[2] = x_bottom_right[i]; // x- Bottom-Right
-                piece_vector[3] = y_bottom_right[i]; // y- Bottom-Right
-                Pieces_positions.Add(piece_vector);
-
-                double rads;
-                if (helper_point_x[i] == x_top_left[i] && helper_point_y[i] == y_top_left[i])
-                {
-                    // No rotation needed
-                    Pieces_angle.Add(0);
-                    angles_rads.Add(0.0);
-                    rads = 0.0;
-                }
-                else
-                {
-                    //Calculate Angle
-                    double oppositive = y_top_left[i] - helper_point_y[i];
-                    double adjancent = helper_point_x[i] - x_top_left[i];
-                    rads = Math.Tanh(oppositive / adjancent);
-                    angles_rads.Add(rads);
-
-                    int angle = RadsToDegrees(rads);
-                    Pieces_angle.Add(angle);
-                }
-
-                images_pieces.Add(GetImagesPieces(originalImage, piece_vector, rads, helper_point_x[i], helper_point_y[i]));
-            }
+            return index;
         }
 
-        const double RadToDegreeFactor = 180.0 / Math.PI;
-        public static int RadsToDegrees(double rads)
+        private int RadsToDegrees(double rads)
         {
             return (int)Math.Round(rads * RadToDegreeFactor);
         }
 
-
-        public static unsafe Image<Bgr, byte> GetImagesPieces(Image<Bgr, byte> originalImage, int[] piece, double angle, int helper_x, int helper_y)
+        private unsafe Image<Bgr, byte> GetImagesPieces(int[] piece, double angle, int helper_x, int helper_y)
         {
             unsafe
             {
-
-                MIplImage original = originalImage.MIplImage;
+                MIplImage original = this.img.MIplImage;
                 byte* dataPtrOriginal = (byte*)original.imageData.ToPointer();
                 int nChan = original.nChannels;
                 int padding = original.widthStep - original.nChannels * original.width;
                 int step = original.widthStep;
-                int width = originalImage.Width;
-                int height = originalImage.Height;
+                int width = this.img.Width;
+                int height = this.img.Height;
 
                 int cols = 0;
                 int rows = 0;
                 if (angle != 0.0)
                 {
-                    cols = (int) Math.Round(Math.Sqrt(Math.Pow(helper_x - piece[0], 2) + Math.Pow(piece[1] - helper_y, 2))) + 1;
-                    rows = (int) Math.Round(Math.Sqrt(Math.Pow(piece[2] - helper_x, 2) + Math.Pow(helper_y - piece[3], 2))) + 1;
+                    cols = (int)Math.Round(Math.Sqrt(Math.Pow(helper_x - piece[0], 2) + Math.Pow(piece[1] - helper_y, 2))) + 1;
+                    rows = (int)Math.Round(Math.Sqrt(Math.Pow(piece[2] - helper_x, 2) + Math.Pow(helper_y - piece[3], 2))) + 1;
                 }
                 else
                 {
                     cols = piece[2] - piece[0] + 1;
                     rows = piece[3] - piece[1] + 1;
                 }
-                
 
                 Image<Bgr, byte> tmp = new Image<Bgr, byte>(cols, rows);
                 MIplImage m = tmp.MIplImage;
@@ -327,33 +227,27 @@ namespace SS_OpenCV
 
                 int x_p = piece[0];
                 int y_p = piece[1];
-
+                int x2;
+                int y2;
 
                 for (int y = 0; y < rows; y++)
                 {
                     for (int x = 0; x < cols; x++)
                     {
-                        if(angle != 0.0)
+                        if (angle != 0.0)
                         {
-                            int dx_diff = (int)Math.Round(x * Math.Cos(-angle) - y * Math.Sin(-angle) + x_p);
-                            int dy_diff = (int)Math.Round(x * Math.Sin(-angle) + y * Math.Cos(-angle) + y_p);
-                            // int dx_diff = (int)Math.Round(((x+x_p) - width / 2.0) * Math.Cos(angle) - (height / 2.0 - (y+y_p)) * Math.Sin(angle) + (width / 2.0));
-                            // int dy_diff = (int)Math.Round(height / 2.0 - ((x_p+x) - width / 2.0) * Math.Sin(angle) - (height / 2.0 - (y_p+y)) * Math.Cos(angle));
-                            int x2 = angle == 0.0 ? x : dx_diff;
-                            int y2 = angle == 0.0 ? y : dy_diff;
-                            newImagePointer[0] = (dataPtrOriginal + ((x2 ) * nChan) + ((y2 ) * step))[0];
-                            newImagePointer[1] = (dataPtrOriginal + ((x2 ) * nChan) + ((y2 ) * step))[1];
-                            newImagePointer[2] = (dataPtrOriginal + ((x2 ) * nChan) + ((y2 ) * step))[2];
+                            x2 = (int)Math.Round(x * Math.Cos(-angle) - y * Math.Sin(-angle) + x_p);
+                            y2 = (int)Math.Round(x * Math.Sin(-angle) + y * Math.Cos(-angle) + y_p);
                         }
                         else
                         {
-                            int x2 = x;
-                            int y2 = y;
-                            newImagePointer[0] = (dataPtrOriginal + ((x2 + x_p) * nChan) + ((y2 + y_p) * step))[0];
-                            newImagePointer[1] = (dataPtrOriginal + ((x2 + x_p) * nChan) + ((y2 + y_p) * step))[1];
-                            newImagePointer[2] = (dataPtrOriginal + ((x2 + x_p) * nChan) + ((y2 + y_p) * step))[2];
+                            x2 = x + x_p;
+                            y2 = y + y_p;
                         }
-                        
+
+                        newImagePointer[0] = (dataPtrOriginal + (x2 * nChan) + (y2 * step))[0];
+                        newImagePointer[1] = (dataPtrOriginal + (x2 * nChan) + (y2 * step))[1];
+                        newImagePointer[2] = (dataPtrOriginal + (x2 * nChan) + (y2 * step))[2];
 
                         newImagePointer += nChan_new;
                     }
@@ -361,8 +255,103 @@ namespace SS_OpenCV
                 }
 
                 return tmp;
-
             }
+        }
+
+        public void getPiecesPosition(out List<int[]> Pieces_positions, out List<int> Pieces_angle)
+        {
+            int width = labels.GetLength(0);
+            int height = labels.GetLength(1);
+
+            Pieces_positions = new List<int[]>();
+            Pieces_angle = new List<int>();
+
+            int[] piece_vector;
+            int[] x_top_left = new int[MaxNumberPieces];
+            int[] y_top_left = new int[MaxNumberPieces];
+            int[] helper_point_x = new int[MaxNumberPieces];
+            int[] helper_point_y = new int[MaxNumberPieces];
+            int[] x_bottom_right = new int[MaxNumberPieces];
+            int[] y_bottom_right = new int[MaxNumberPieces];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (labels[x, y] != UInt32.MaxValue)
+                    {
+                        if (labels[x, y - 1] == UInt32.MaxValue && labels[x - 1, y] == UInt32.MaxValue && labels[x - 1, y + 2] == UInt32.MaxValue)
+                        {
+                            // Found a top left corner
+                            int index = getLabelIndex(labels[x, y]);
+
+                            x_top_left[index] = x;
+                            y_top_left[index] = y;
+
+                            Console.WriteLine("Label: " + labels[x, y]);
+                            Console.WriteLine("Top Left X: " + x);
+                            Console.WriteLine("Top Left Y: " + y);
+
+                        }
+                        else if (labels[x, y + 1] == UInt32.MaxValue && labels[x + 1, y] == UInt32.MaxValue && labels[x + 1, y - 2] == UInt32.MaxValue)
+                        {
+                            // Found a bottom right corner
+                            int index = getLabelIndex(labels[x, y]);
+                            x_bottom_right[index] = x;
+                            y_bottom_right[index] = y;
+
+                            Console.WriteLine("Label: " + labels[x, y]);
+                            Console.WriteLine("Bottom Right X: " + x);
+                            Console.WriteLine("Bottom Right Y: " + y);
+                        }
+                        else if (labels[x, y - 1] == UInt32.MaxValue && labels[x + 1, y] == UInt32.MaxValue && labels[x - 2, y - 1] == UInt32.MaxValue)
+                        {
+                            // Found a top right corner (helper)
+                            int index = getLabelIndex(labels[x, y]);
+
+                            helper_point_x[index] = x;
+                            helper_point_y[index] = y;
+
+                            Console.WriteLine("Label: " + labels[x, y]);
+                            Console.WriteLine("Top Right X: " + x);
+                            Console.WriteLine("Top Right Y: " + y);
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < this.curr_label_index; i++)
+            {
+                piece_vector = new int[4];
+                piece_vector[0] = x_top_left[i];   // x- Top-Left 
+                piece_vector[1] = y_top_left[i];  // y- Top-Left
+                piece_vector[2] = x_bottom_right[i]; // x- Bottom-Right
+                piece_vector[3] = y_bottom_right[i]; // y- Bottom-Right
+                Pieces_positions.Add(piece_vector);
+                double rads;
+                if (helper_point_x[i] == x_top_left[i] && helper_point_y[i] == y_top_left[i])
+                {
+                    // No rotation needed
+                    Pieces_angle.Add(0);
+                    rads = 0.0;
+                }
+                else
+                {
+                    //Calculate Angle
+                    double oppositive = y_top_left[i] - helper_point_y[i];
+                    double adjancent = helper_point_x[i] - x_top_left[i];
+                    rads = Math.Tanh(oppositive / adjancent);
+                    Console.WriteLine("Angle: " + this.RadsToDegrees(rads));
+                    Pieces_angle.Add(this.RadsToDegrees(rads));
+                }
+
+                images_pieces.Add(this.GetImagesPieces(piece_vector, rads, helper_point_x[i], helper_point_y[i]));
+            }
+        }
+
+        public Image<Bgr, byte> getFinalImage()
+        {
+            return images_pieces[1];
         }
 
     }
